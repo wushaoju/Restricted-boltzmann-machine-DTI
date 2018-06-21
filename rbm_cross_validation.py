181163455daa23b9745535cd3cf26eab17a4aabe
wushaoju <632750290@qq.com>
2018-06-21 14:36:37 -0400

    Add files via upload

--- rbm_cross_validation.py
+++ rbm_cross_validation.py
@@ -4,6 +4,8 @@ Created on Tue Jun 19 01:51:23 2018
 DTI concussion classification: general control(77 cases) vs mild TBI(130 cases) 
 with FA, MD, AD, RD four modailty
 Using Restricted-boltzmann machine with cross validation
+Reference: Yunliang Cai, Songbai Ji, "Combining Deep Learning Networks with Permutation Tests to Predict Traumatic Brain Injury Outcome", 
+MICCAI Grand Challenge Workshop, Athens, Greece, 2016, Springer.
 @author: Shaoju Wu
 """
 import numpy as np
@@ -16,6 +18,7 @@ from sklearn import svm
 from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import KFold
 from sklearn.model_selection import StratifiedKFold
+from sklearn.linear_model import LogisticRegression
 from scipy.io import loadmat
 import os
 
@@ -66,7 +69,28 @@ def make_GBRBMtransform_kfold(train_data, valid_data, Image_type, k, save_path):
     transform_data_val = gbrbm.transform(valid_data)      
     return transform_data_train, transform_data_val
 
-def make_BBRBM_kfold(train_data, valid_data, Image_type, k, save_path):     
+def make_BBRBM_kfold(train_data, valid_data, num_visible, num_hidden, num_epoches, k, save_path, layer_num):
+     ##############% Transform the data  with Bernoulli restricted Boltzmann machine
+    bbrbm = BBRBM(n_visible=num_visible, n_hidden = num_hidden, learning_rate = 0.001, momentum=0.9, use_tqdm=True)    
+    foldName = str(k)+'_fold/'+'layer'+str(layer_num)+'/'
+    createFolder(save_path+foldName)
+    if os.listdir(save_path+foldName):
+        bbrbm.load_weights(filename = save_path+foldName, name = 'layer'+str(layer_num)+'_model')
+    else : 
+        errs,errs_val = bbrbm.fit(train_data, valid_data, n_epoches = num_epoches, batch_size=20) 
+        plt.plot(errs)
+        plt.show()
+        plt.savefig(save_path+foldName+'train.png')
+        plt.plot(errs_val)
+        plt.savefig(save_path+foldName+'val.png')
+        plt.show()
+        bbrbm.save_weights(filename = save_path+foldName, name = 'layer'+str(layer_num)+'_model')
+    transform_data_train = np.zeros([train_data.shape[0] , num_hidden]).astype(np.float32)
+    for i in range(0,train_data.shape[0]):
+        transform_data_train[i,:] = bbrbm.transform(train_data[i,:].reshape(1,-1))
+    
+    transform_data_val = bbrbm.transform(valid_data)      
+    return transform_data_train, transform_data_val    
     
 
 def createFolder(directory):
@@ -137,32 +161,52 @@ for loop in range(0,n_trial):
             make_GBRBM_kfold(X_train, X_test, 'MD', index, save_path)
             
         with tf.Graph().as_default():
-             X_train_t, X_test_t = make_GBRBMtransform_kfold(X_train, X_test, 'MD', index, save_path)
-        clf2 = svm.SVC(kernel='linear',probability=True)     
-#        clf2 = RandomForestClassifier(45, max_depth=5, max_features=None, min_samples_split=2)
-        clf2.fit(X_train_t, Y_train)
-        SVM_Accuracy[index] = clf2.score(X_test_t, Y_test)
-        Y_predict = clf2.predict(X_test_t)
-        tn, fp, fn, tp = confusion_matrix(Y_test, Y_predict).ravel()
-        SVM_Sensitivity[index] = np.float(tp)/np.float(tp+fn)
-        SVM_Specificity[index] = np.float(tn)/np.float(tn+fp)
+             X_train_MD, X_test_MD = make_GBRBMtransform_kfold(X_train, X_test, 'MD', index, save_path)
           
-        X_train, X_test, Y_train, Y_test = FA_data[train], FA_data[test], Label[train], Label[test]
+        X_train, X_test, Y_train, Y_test = FA_data[train], FA_data[test], Label[train], Label[test]  
+           
         with tf.Graph().as_default():
-             X_train_t, X_test_t = make_GBRBMtransform_kfold(X_train, X_test, 'FA', index, save_path)
-             
+             make_GBRBM_kfold(X_train, X_test, 'FA', index, save_path)
+            
         with tf.Graph().as_default():
-            make_GBRBM_kfold(X_train, X_test, 'FA', index, save_path)
+             X_train_FA, X_test_FA = make_GBRBMtransform_kfold(X_train, X_test, 'FA', index, save_path)
 
         X_train, X_test, Y_train, Y_test = AD_norm[train], AD_norm[test], Label[train], Label[test]
         with tf.Graph().as_default():
-            make_GBRBM_kfold(X_train, X_test, 'AD', index, save_path)
+             make_GBRBM_kfold(X_train, X_test, 'AD', index, save_path)
+             X_train_AD, X_test_AD = make_GBRBMtransform_kfold(X_train, X_test, 'AD', index, save_path)
+            
 
         X_train, X_test, Y_train, Y_test = RD_norm[train], RD_norm[test], Label[train], Label[test]
         with tf.Graph().as_default():
-            make_GBRBM_kfold(X_train, X_test, 'RD', index, save_path)
-        
+             make_GBRBM_kfold(X_train, X_test, 'RD', index, save_path)
+             X_train_RD, X_test_RD = make_GBRBMtransform_kfold(X_train, X_test, 'RD', index, save_path)
             
+             
+        X_train_l2 = np.concatenate((X_train_FA, X_train_MD), axis=1)
+        X_train_l2 = np.concatenate((X_train_l2, X_train_AD), axis=1)
+        X_train_l2 = np.concatenate((X_train_l2, X_train_RD), axis=1)
+                
+        X_test_l2 = np.concatenate((X_test_FA, X_test_MD), axis=1)
+        X_test_l2 = np.concatenate((X_test_l2, X_test_AD), axis=1)
+        X_test_l2 = np.concatenate((X_test_l2, X_test_RD), axis=1)
+        with tf.Graph().as_default():
+            X_train_2, X_test_2 = make_BBRBM_kfold(X_train_l2, X_test_l2, 4000, 5000, 450, index, save_path, 2)
+        with tf.Graph().as_default():    
+            X_train_3, X_test_3 = make_BBRBM_kfold(X_train_2, X_test_2, 5000, 2000, 640, index, save_path, 3)
+        with tf.Graph().as_default():    
+            X_train_4, X_test_4 = make_BBRBM_kfold(X_train_3, X_test_3, 2000, 1000, 540, index, save_path, 4)
+        with tf.Graph().as_default():    
+            X_train_5, X_test_5 = make_BBRBM_kfold(X_train_4, X_test_4, 1000, 200, 1024, index, save_path, 5) 
+            
+#        clf2 = RandomForestClassifier(45, max_depth=5, max_features=None, min_samples_split=2)
+        clf2 = svm.SVC(kernel='linear',probability=True)    
+        clf2.fit(X_train_4, Y_train)
+        SVM_Accuracy[index] = clf2.score(X_test_4, Y_test)
+        Y_predict = clf2.predict(X_test_4)
+        tn, fp, fn, tp = confusion_matrix(Y_test, Y_predict).ravel()
+        SVM_Sensitivity[index] = np.float(tp)/np.float(tp+fn)
+        SVM_Specificity[index] = np.float(tn)/np.float(tn+fp)            
         index=index+1
 
     Total_mean_acc[loop] = np.mean(SVM_Accuracy,axis=0)
